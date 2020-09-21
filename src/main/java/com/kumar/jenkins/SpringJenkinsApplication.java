1ce7dd7e59f0649b0dd66a87bd15665d359d4dc4
rkumar <rakesh4vi@gmail.com>
2020-09-21 09:19:24 +0530

    first commit for jenkins

---
+++ src/main/java/com/kumar/jenkins/SpringJenkinsApplication.java
@@ -0,0 +1,24 @@
+package com.kumar.jenkins;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsApplication {
+
+	public static Logger logger=LoggerFactory.getLogger(SpringJenkinsApplication.class);
+	
+	@PostConstruct
+	public void init() {
+		logger.info(":::::App is started::::");
+	}
+	public static void main(String[] args) {
+		logger.info(":::::App is executing::::");
+		SpringApplication.run(SpringJenkinsApplication.class, args);
+	}
+
+}
